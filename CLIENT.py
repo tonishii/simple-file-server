d267034a0cd3ddf038fca528d7476b838176021f
tonishii <anthonylibut@gmail.com>
2024-12-06 16:16:08 +0800

    Added the final comments.

--- CLIENT.py
+++ CLIENT.py
@@ -2,6 +2,7 @@ import socket
 import ipaddress
 import os
 
+# Ensure that tqdm loads properly else the program will not use it
 try:
     from tqdm import tqdm
     tqdm_available = True
@@ -9,7 +10,6 @@ except ImportError:
     tqdm = None
     tqdm_available = False
 
-client_socket = None
 BUFFER = 1024
 FORMAT = "utf-8"
 
@@ -75,65 +75,72 @@ def is_filename(filename: str) -> bool:
 def print_commands() -> None:
     """Print the available commands in the application."""
 
-    print("\nCommands:")
+    print("Commands:")
     for cmd, desc in commands.items():
         print(f"   {cmd.ljust(30)} {desc}")
     print()
 
-def send_file(filename: str, client_socket: socket.socket) -> None:
+def send_file(client_socket: socket.socket, filename: str) -> None:
     """Sends a file to the specified socket.
 
-    :param str reg: register of the client
     :param socket.socket client_socket: current socket used by the client
+    :param str reg: register of the client
     """
 
-    # Check if the file exists in the current directory
-
+    # Send the file size to the server
     filesize = os.path.getsize(filename)
     client_socket.sendall(f"{filesize}".ljust(BUFFER).encode(FORMAT))
 
+    # Load the progress bar
     if tqdm_available:
         progress_bar = tqdm(total=filesize, unit="B", unit_scale=True, unit_divisor=1024, desc="Sending")
 
-    # Send the size of the file
     with open(filename, "rb") as f:
         while bytes_read := f.read(BUFFER):
-            # read the bytes from the file
+            # Read the bytes from the file until EOF
             client_socket.sendall(bytes_read)
 
+            # Update the progress bar
             if tqdm_available:
                 progress_bar.update(len(bytes_read))
 
     if tqdm_available:
         progress_bar.close()
 
+    # Print the message received from the server
     print(recv_data(client_socket).decode(FORMAT))
 
-def receive_file(filename: str, client_socket: socket.socket) -> None:
-    """Receives a file from the specified socket and write it to a file.
+def store_file(client_socket: socket.socket, filename: str) -> None:
+    """Receives a file from the specified socket and writes it to a file in the user's directory.
 
-    :param str filename: filename to be received from the server
     :param socket.socket client_socket: current socket used by the client
+    :param str filename: filename to be received from the server
     """
 
+    # Server sends a 0 status flag if the file is not in the server
     if client_socket.recv(1) == b'0':
         print("Error: File not found in the server.")
         return
 
+    # Get the file size to be received from the server
     received, filesize = 0, int(client_socket.recv(BUFFER).decode(FORMAT))
 
+    # Load the progress bar
     if tqdm_available:
         progress_bar = tqdm(total=filesize, unit="B", unit_scale=True, unit_divisor=1024, desc="Receiving")
 
     with open(filename, "wb") as f:
         while received < filesize:
+            # Read the bytes from the file until all bytes are received
             bytes_read = client_socket.recv(BUFFER)
-            received += len(bytes_read)
 
+            # Write to the file
             f.write(bytes_read)
 
+            # Update the progress bar
             if tqdm_available:
                 progress_bar.update(len(bytes_read))
+            received += len(bytes_read)
 
     if tqdm_available:
         progress_bar.close()
@@ -146,7 +153,7 @@ def execute_command(client_socket: socket.socket, reg: str, command: str) -> str
     :param socket.socket client_socket: current socket used by the client
     :param str reg: register of the client
     :param str command: command given by the user
-    :return str: returns the register/handle/alias of the client at the execution of the command
+    :return str: returns the current register/handle/alias of the client at the execution of the command
     """
 
     if command.startswith('/register'):
@@ -158,17 +165,17 @@ def execute_command(client_socket: socket.socket, reg: str, command: str) -> str
 
         if len(parts) < 2:
             print("Error: Command parameters do not match or is not allowed")
-            return reg
+            return None
 
         command, handle = parts
 
         client_socket.sendall(command.encode(FORMAT))
-
         client_socket.sendall(encode_to_bytes(handle))
         client_socket.sendall(handle.encode(FORMAT))
 
         status = client_socket.recv(1)
 
+        # Get the status flag, server sends a 1 if handle is valid else 0
         if status == b'1':
             print(f"Welcome {handle}!")
             return handle
@@ -178,60 +185,64 @@ def execute_command(client_socket: socket.socket, reg: str, command: str) -> str
             return None
 
     elif command.startswith('/store'):
-        if reg is not None:
-            parts = command.split(" ", 1)
-
-            if len(parts) < 2:
-                print("Error: Command parameters do not match or is not allowed")
-                return reg
-
-            command, filename = parts
-
-            if not os.path.exists(filename):
-                print("Error: File not found.")
-                return reg
-
-            if not is_filename(filename):
-                print(f"Error: Invalid filename {filename}.")
-                return reg
-
-            client_socket.sendall(command.encode(FORMAT))
-            client_socket.sendall(encode_to_bytes(filename))
-            client_socket.sendall(filename.encode(FORMAT))
-
-            send_file(filename, client_socket)
-        else:
+        if reg is None:
             print("Error: No register entered in the server.")
+            return None
+
+        parts = command.split(" ", 1)
+
+        if len(parts) < 2:
+            print("Error: Command parameters do not match or is not allowed")
+            return reg
+
+        command, filename = parts
+
+        if not os.path.exists(filename):
+            print("Error: File not found.")
+            return reg
+
+        if not is_filename(filename):
+            print(f"Error: Invalid filename {filename}.")
+            return reg
+
+        client_socket.sendall(command.encode(FORMAT))
+        client_socket.sendall(encode_to_bytes(filename))
+        client_socket.sendall(filename.encode(FORMAT))
+
+        send_file(client_socket, filename)
 
     elif command.startswith('/get'):
-        if reg is not None:
-            parts = command.split(" ", 1)
-
-            if len(parts) < 2:
-                print("Error: Command parameters do not match or is not allowed")
-                return reg
-
-            command, filename = parts
-
-            if not is_filename(filename):
-                print(f"Error: Invalid filename {filename}.")
-                return reg
-
-            client_socket.sendall(command.encode(FORMAT))
-            client_socket.sendall(encode_to_bytes(filename))
-            client_socket.sendall(filename.encode(FORMAT))
-
-            receive_file(filename, client_socket)
-        else:
+        if reg is None:
             print("Error: No register entered in the server")
+            return None
+
+        parts = command.split(" ", 1)
+
+        if len(parts) < 2:
+            print("Error: Command parameters do not match or is not allowed")
+            return reg
+
+        command, filename = parts
+
+        if not is_filename(filename):
+            print(f"Error: Invalid filename {filename}.")
+            return reg
+
+        client_socket.sendall(command.encode(FORMAT))
+        client_socket.sendall(encode_to_bytes(filename))
+        client_socket.sendall(filename.encode(FORMAT))
+
+        store_file(client_socket, filename)
 
     elif command == '/dir':
-        if reg is not None:
-            client_socket.sendall(command.encode(FORMAT))
-
-            print(recv_data(client_socket).decode(FORMAT))
-        else:
+        if reg is None:
             print("Error: No register entered in the server.")
+            return None
+
+        client_socket.sendall(command.encode(FORMAT))
+
+        # Prints the directory received from the server
+        print(recv_data(client_socket).decode(FORMAT))
 
     elif command == '/leave':
         client_socket.sendall(command.encode(FORMAT))
@@ -244,6 +255,7 @@ def execute_command(client_socket: socket.socket, reg: str, command: str) -> str
 
 def handle_commands() -> None:
     """Handles the input given by the user
+
     :raises ValueError: raises this error when invalid parameters were met when inputting the command
     """
     client_socket, reg, active = None, None, False
@@ -253,37 +265,42 @@ def handle_commands() -> None:
         print()
 
         if command.startswith('/join') and not active:
-            # Check if HOST and PORT is valid
             try:
                 _, HOST, PORT = command.split(" ", 2)
                 PORT = int(PORT)
 
+                # Check if HOST and PORT is valid
                 if not is_valid_addr(HOST) or not is_valid_port(PORT):
                     raise ValueError
 
+                # Connect to the address
                 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 client_socket.connect((HOST, PORT))
 
+                # Set the active status flag to active or True
                 active = True
                 print("Connected to the File Exchange Server successfully!")
 
-            except (ValueError, OSError) as e:
+            except (ValueError, OSError):
                 print("Error: Connection to the Server has failed! Please check IP Address and Port Number.")
 
         elif command == '/exit':
             print("Exiting application!")
-            break
+            break # Break from the inf. loop
 
+        # Do not allow multiple joins to different or the same server
         elif command.startswith('/join') and active:
             print("Error: Still connected to server. Leave from the server first.")
 
         elif command == '/?':
             print_commands()
 
+        # Do not allow the client to leave a server if not connected
         elif not active and command == '/leave':
             print("Error: Disconnection failed. Please connect to the server first.")
 
         elif active:
+            # Execute the command and get the register
             reg = execute_command(client_socket, reg, command)
 
             if reg is None and command == '/leave':
@@ -293,6 +310,7 @@ def handle_commands() -> None:
                 client_socket.close()
                 client_socket = None
 
+        # Check if the command is not in any of the active commands used
         elif any(command.startswith(key) for key in commands.keys()):
             print("Error: Command not found.")
 
@@ -302,7 +320,7 @@ def handle_commands() -> None:
 if __name__ == "__main__":
     try:
         handle_commands()
-    except ConnectionError:
+    except ConnectionError: # Happens ie. when the server suddenly shutsdown
         print("Error: Server side connection abruptly closed")
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: # Happens ie. when user accidentally used Ctrl + C on the terminal
         print("Connection closed. Thank you!")
--- SERVER.py
+++ SERVER.py
@@ -8,6 +8,8 @@ from typing import Optional
 BUFFER = 1024
 FORMAT = "utf-8"
 DIRECTORY = 'SERVERFILES'
+
+# Stores the active registers in the server
 registers = []
 
 # Get the designated host and port
@@ -38,6 +40,32 @@ def recv_data(client_socket: socket.socket) -> bytes:
     data_length = int(client_socket.recv(BUFFER).decode(FORMAT))
     return client_socket.recv(data_length)
 
+def send_file(client_socket: socket.socket, filename: str) -> None:
+    """Sends the file through the connection of the client.
+
+    :param socket.socket client_socket: current connection of the client
+    :param str filename: name of the file to be sent to the client connection
+    """
+
+    # Get the filepath to the servers DIRECTORY
+    filepath = os.path.join(DIRECTORY, filename)
+
+    if os.path.exists(filepath):
+        # Send the OK status flag to the client
+        client_socket.sendall(b'1')
+
+        # Send the file size to the client
+        file_size = os.path.getsize(filepath)
+        client_socket.sendall(f"{file_size}".ljust(BUFFER).encode(FORMAT))
+
+        with open(filepath, "rb") as f:
+            while bytes_read := f.read(BUFFER):
+                # Read the bytes from the file until EOF
+                client_socket.sendall(bytes_read)
+    else:
+        # Send the 0 status flag to the client
+        client_socket.sendall(b'0')
+
 def store_file(client_socket: socket.socket, filename: str, reg: str) -> None:
     """Stores the file in the directory of the server.
 
@@ -46,22 +74,31 @@ def store_file(client_socket: socket.socket, filename: str, reg: str) -> None:
     :param str reg: handle of the currently connected client
     """
 
+    # Get the filepath to the servers DIRECTORY
     filepath = os.path.join(DIRECTORY, filename)
+
+    # Get the file size to be received from the client
     received, filesize = 0, int(client_socket.recv(BUFFER).decode(FORMAT))
 
     try:
         with open(filepath, "wb") as f:
             while received < filesize:
+                # Read the bytes from the file until all bytes are received
                 bytes_read = client_socket.recv(BUFFER)
+
                 received += len(bytes_read)
+
+                # Write to the file
                 f.write(bytes_read)
 
+        # Print confirmation message and send respone to the client
         print(f"[DIRECTORY] {reg} has uploaded {filename} {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.")
         response = f"{reg}{datetime.now().strftime("<%Y-%m-%d %H:%M:%S>")}:\nUploaded\n{filename}"
         client_socket.sendall(encode_to_bytes(response))
         client_socket.sendall(response.encode(FORMAT))
 
     except:
+        # Send error response to the client
         response = "Error: File receiving aborted."
         client_socket.sendall(encode_to_bytes(response))
         client_socket.sendall(response.encode(FORMAT))
@@ -72,58 +109,52 @@ def get_dir(client_socket: socket.socket) -> None:
     :param socket.socket client_socket: current connection of the client
     """
 
+    # Get the servers directory
     file_list = os.listdir(DIRECTORY)
-    msg = "Server Directory\n" + "\n".join(file_list) if file_list else "Server directory is empty"
+
+    # Join everything using a new line else if empty then the default string/msg
+    msg = "Server Directory\n" + "\n".join(file_list) if file_list else "Server Directory is empty"
 
     client_socket.sendall(encode_to_bytes(msg))
     client_socket.sendall(msg.encode(FORMAT))
 
-def send_file(client_socket: socket.socket, filename: str) -> None:
-    """Sends the file through the connection of the client
+def register_client(client_socket: socket.socket, reg: str) -> Optional[str]:
+    """Registers the clients handle received by the server
 
     :param socket.socket client_socket: current connection of the client
-    :param str filename: name of the file to be sent to the client connection
+    :param str reg: handle/register received by the server
+    :return Optional[str]: return the clients handle, None if invalid
     """
 
-    filepath = os.path.join(DIRECTORY, filename)
-
-    if os.path.exists(filepath):
-        client_socket.sendall(b'1')
-
-        # Send the size of the file
-        with open(filepath, "rb") as f:
-            file_size = os.path.getsize(filepath)
-            client_socket.sendall(f"{file_size}".ljust(BUFFER).encode(FORMAT))
-
-            while bytes_read := f.read(BUFFER):
-                # read the bytes from the file
-                client_socket.sendall(bytes_read)
-    else:
-        client_socket.sendall(b'0')
-
-def register_client(client_socket: socket.socket, reg: str) -> Optional[str]:
     if reg in registers:
+        # Send 0 status flag if handle already exists
         client_socket.sendall(b'0')
         return None
 
+    # Add the handle to the active registers
     registers.append(reg)
     print(f"[CONNECTION] Active registers: {", ".join(registers)}")
     print(f"[CONNECTION] Register added to the server: {reg}.")
+
+    # Send 1 status flag if handle is valid
     client_socket.sendall(b'1')
 
     return reg
 
 def handle_client(client_socket: socket.socket, addr: str) -> None:
-    reg = None
+    reg = None # Holds the active/current handle of the client
+
     while True:
         try:
+            # Get the request and the ff. data used in various commands
             request = client_socket.recv(BUFFER).decode(FORMAT)
 
             if request not in ["/dir", "/leave"]:
                 data = recv_data(client_socket)
 
-            # print(request, data, reg)
+            # print(request, data, reg) # FOR CHECKING
 
+            # Send to the appropriate command handlers
             match request:
                 case "/register": reg = register_client(client_socket, data.decode(FORMAT))
                 case "/get": send_file(client_socket, data.decode(FORMAT))
@@ -132,14 +163,15 @@ def handle_client(client_socket: socket.socket, addr: str) -> None:
                 case "/leave":
                     break
 
-        except ConnectionError:
+        except ConnectionError: # Happens ie. when the client was suddenly shutdown
             print("[ERROR] Connection abruptly closed. Disconnecting client.")
             break
 
-        except ValueError:
+        except ValueError: # Happens ie. when the client exits without using the /leave command
             print("[ERROR] Unknown request/data received. Disconnecting client.")
             break
 
+    # Only remove reg when client was registered in the server
     if reg:
         registers.remove(reg)
 
@@ -147,15 +179,16 @@ def handle_client(client_socket: socket.socket, addr: str) -> None:
     client_socket.close()
 
 if __name__ == "__main__":
-    active = True
+    # Activate the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         print(f"[STARTING] Binding server with address and port number: {SERVER_HOST} {SERVER_PORT}")
         print("[STARTING] Server is accepting connections.")
 
+        # Bind to the address
         server.bind((SERVER_HOST, SERVER_PORT))
         server.listen()
 
-        while active:
+        while True:
             conn, addr = server.accept()
             print(f"[CONNECTION] Client connected to server: {addr[0]} {addr[1]}.")
             print(f"[CONNECTION] Active connections: {threading.active_count()}.")
